e4444e2c2ebf994f185d092c829d619d0b13e37e
Yi Li <yl29@princeton.edu>
2020-03-05 11:51:14 -0500

    sim_main.cc update

--- app/sim_main.cc
+++ app/sim_main.cc
@@ -10,58 +10,47 @@
 SC_MODULE(Source) {
   sc_in<bool> clk{"clk"};
   //sc_in<bool> rst;
-  
-  sc_out< sc_biguint<1> > flex_wr_in;
-  sc_out< sc_biguint<1> > flex_rd_in;
-  sc_out< sc_biguint<32> > flex_addr_in;
-  sc_out< sc_biguint<8> > flex_data_in[16];
 
-  sc_in< sc_biguint<1> > relay_sim_relay_func_fun_in_in;
-  sc_biguint<1> relay_sim_relay_func_fun_in;
-  sc_in< sc_biguint<8> > relay_sim_relay_func_id_in;
-  sc_biguint<8> relay_sim_relay_func_id;
-  sc_in< sc_biguint<8> > relay_sim_relay_data_in_in;
-  sc_biguint<8> relay_sim_relay_data_in;
-  sc_in< sc_biguint<8> > relay_sim_data_in_batch_in;
-  sc_biguint<8> relay_sim_data_in_batch;
-  sc_in< sc_biguint<8> > relay_sim_data_in_channel_in;
-  sc_biguint<8> relay_sim_data_in_channel;
-  sc_in< sc_biguint<32> > relay_sim_data_in_y_in;
-  sc_biguint<32> relay_sim_data_in_y;
-  sc_in< sc_biguint<32> > relay_sim_data_in_x_in;
-  sc_biguint<32> relay_sim_data_in_x;
-  sc_in< sc_biguint<8> > relay_sim_pool_size_y_in;
-  sc_biguint<8> relay_sim_pool_size_y;
-  sc_in< sc_biguint<8> > relay_sim_pool_size_x_in;
-  sc_biguint<8> relay_sim_pool_size_x;
-  sc_in< sc_biguint<8> > relay_sim_strides_y_in_in;
-  sc_biguint<8> relay_sim_strides_y_in;
-  sc_in< sc_biguint<8> > relay_sim_strides_x_in_in;
-  sc_biguint<8> relay_sim_strides_x_in;
-  sc_in< sc_biguint<8> > relay_sim_padding_in_y_in;
-  sc_biguint<8> relay_sim_padding_in_y;
-  sc_in< sc_biguint<8> > relay_sim_padding_in_x_in;
-  sc_biguint<8> relay_sim_padding_in_x;
-  sc_in< sc_biguint<8> > relay_sim_layout_in_in;
-  sc_biguint<8> relay_sim_layout_in;
-  sc_in< sc_biguint<8> > relay_sim_ceil_mode_in_in;
+  sc_out< sc_biguint<1> > relay_sim_relay_func_run_in_in;
+  sc_out< sc_biguint<8> > relay_sim_relay_func_id_in;
+  sc_out< sc_biguint<8> > relay_sim_relay_data_in_in;
+  sc_out< sc_biguint<8> > relay_sim_data_in_batch_in;
+  sc_out< sc_biguint<8> > relay_sim_data_in_channel_in;
+  sc_out< sc_biguint<32> > relay_sim_data_in_y_in;
+  sc_out< sc_biguint<32> > relay_sim_data_in_x_in;
+  sc_out< sc_biguint<8> > relay_sim_pool_size_y_in;
+  sc_out< sc_biguint<8> > relay_sim_pool_size_x_in;
+  sc_out< sc_biguint<8> > relay_sim_strides_y_in_in;
+  sc_out< sc_biguint<8> > relay_sim_strides_x_in_in;
+  sc_out< sc_biguint<8> > relay_sim_padding_in_y_in;
+  sc_out< sc_biguint<8> > relay_sim_padding_in_x_in;
+  sc_out< sc_biguint<8> > relay_sim_layout_in_in;
+  sc_out< sc_biguint<8> > relay_sim_ceil_mode_in_in;
 
 //  void source_input();
 
   SC_CTOR(Source) {
     SC_THREAD(source_input);
     sensitive << clk.pos();
-    //reset_signal_is(rst, true);
   }
 
   void source_input() {
     // reset
-    flex_wr_in = 0;
-    flex_rd_in = 0;
-    flex_addr_in = "0x0";
-    for (int i = 0; i < 16; i++) {
-      flex_data_in[i] = "0x0";
-    }
+    relay_sim_relay_func_run_in_in = 0;
+    relay_sim_relay_func_id_in = 0;
+    relay_sim_relay_data_in_in = 0;
+    relay_sim_data_in_batch_in = 0;
+    relay_sim_data_in_channel_in = 0;
+    relay_sim_data_in_y_in = 0;
+    relay_sim_data_in_x_in = 0;
+    relay_sim_pool_size_y_in = 0;
+    relay_sim_pool_size_x_in = 0;
+    relay_sim_strides_y_in_in = 0;
+    relay_sim_strides_x_in_in = 0;
+    relay_sim_padding_in_y_in = 0;
+    relay_sim_padding_in_x_in = 0;
+    relay_sim_layout_in_in = 0;
+    relay_sim_ceil_mode_in_in = 0;
 
     wait(10, SC_NS);
 
@@ -71,8 +60,19 @@ SC_MODULE(Source) {
     std::string temp;
     std::string hex_hdr = "0x";
     std::string addr_format;
+
+    std::string addr_x, addr_y;
+    std::string addr_x_format, addr_y_format;
+
+    // relay input temp
+    std::string func_run, func_id;
+    std::string data_in;
+
+    std::string data_in_y, data_in_x;
+    std::string pool_y, pool_x;
+    std::string stride_y, stride_x;
     
-    const char *addr_c;
+    const char *addr_x_c, *addr_y_c;
     const char *data_byte_c;
 
     wait(10, SC_NS);
@@ -80,59 +80,82 @@ SC_MODULE(Source) {
     fin.open("/u/yl29/3LA/test_input.csv", ios::in);
 
     while(std::getline(fin, temp, ',')) {
-      std::getline(fin, mode, ',');
-      std::getline(fin, addr, ',');
-      std::getline(fin, data, '\n');
+      std::getline(fin, func_run, ',');
+      std::getline(fin, func_id, ',');
 
-      // cout << mode << '\t' << addr << '\t' << data << endl;
-      // set the WR/RD mode
-      if (mode.compare("W") == 0) {
-        flex_wr_in = 1;
-        flex_rd_in = 0;
-      } else {
-        flex_wr_in = 0;
-        flex_rd_in = 1;
-      }
-      // extract the address
-      addr = addr.substr(2);
-      addr_format = "0x00" + addr;
-      addr_c = addr_format.c_str();
-      flex_addr_in = addr_c;
+      if (func_id.compare("1") == 0) { // function maxpooling
+        std::getline(fin, data_in, ',');
+        std::getline(fin, data_in_y, ',');
+        std::getline(fin, data_in_x, ',');
+        std::getline(fin, pool_y, ',');
+        std::getline(fin, pool_x, ',');
+        std::getline(fin, stride_y, ',');
+        std::getline(fin, stride_x, '\n');
 
-      // extract the data
-      data_format.clear();
-      if (data.length() <= 34) {
-        data_format.append(34 - data.length(), '0');
-        data_format.append(data.substr(2));
-      } else {
-        data_format.append(data.substr(data.length()-32));
+
+      } else { // function tensor store
+        std::getline(fin, data_in, ',');
+        std::getline(fin, data_in_x, '\n');
       }
 
-      std::string data_byte;
-      for (int i = 0; i<16; i++) {
-        data_byte = data_format.substr(30-2*i, 2);
-        data_byte_c = ("0x" + data_byte).c_str();
- //       cout << data_byte << ' ';
-        flex_data_in[i] = data_byte_c;
+      relay_sim_relay_func_run_in_in = func_run.c_str();
+      relay_sim_relay_func_id_in = func_id.c_str();
+
+      // tensor store
+      if (func_id.compare("2") == 0) {
+        //TODO: adapt the correct format of addr
+        // extract the address
+        addr_x = data_in_x.substr(data_in_x.length() - 5, 5);
+        addr_x_format = "0x00" + addr_x;
+        addr_x_c = addr_format.c_str();
+        relay_sim_data_in_x_in = addr_x_c;
+
+        // extract the data
+        data_format.clear();
+        if (data.length() <= 34) {
+          data_format.append(34 - data.length(), '0');
+          data_format.append(data.substr(2));
+        } else {
+          data_format.append(data.substr(data.length()-32));
+        }
+        // TODO: modify here
+        std::string data_byte;
+        for (int i = 0; i<16; i++) {
+          data_byte = data_format.substr(30-2*i, 2);
+          data_byte_c = ("0x" + data_byte).c_str();
+
+          relay_sim_relay_data_in_in = data_byte_c;
+          relay_sim_data_in_x_in = relay_sim_data_in_x_in + i;
+          wait(1, SC_NS);
+        }
       }
-//      cout << endl;
-      
-//       cout << "@" << sc_time_stamp() << " :" << '\t' 
-//            << "mode:" << mode << '\t'
-//            << "addr:" << addr_c << '\t'
-//            << "data:" << data_format << '\t';
-// //      cout << "sc_addr" << '\t'
-// //           << hex << flex_addr_in << '\t'; 
-// //           << "sc_mode" << '\t'
-// //           << flex_wr_in << '\t'
-// //           << flex_rd_in << endl;
-// //      for (int j = 0; j < 16; j++) {
-// //          cout << hex << flex_data_in[15-j] << ' ';
-// //      }
-//       cout << endl;
+
+      // maxpooling
+      if (func_id.compare("1") == 0) {
+    
+        // extract the address
+        addr_x = data_in_x.substr(data_in_x.length() - 5, 5);
+        addr_x_format = "0x00" + addr_x;
+        addr_x_c = addr_format.c_str();
+
+        addr_y = data_in_y.substr(data_in_y.length() - 5, 5);
+        addr_y_format = "0x00" + addr_y;
+        addr_y_c = addr_format.c_str();
+
+        // pass the data to the port
+        relay_sim_relay_data_in_in = 0;
+        relay_sim_data_in_y_in = addr_y_c;
+        relay_sim_data_in_x_in = addr_x_c;
+        relay_sim_pool_size_y_in = pool_y.c_str();
+        relay_sim_pool_size_x_in = pool_x.c_str();
+        relay_sim_strides_y_in_in = stride_y.c_str();
+        relay_sim_strides_x_in_in = stride_x.c_str();
+
+        std::cout << "relay maxpooling instr passed!" << std::endl;
+      }
 
 
-      wait(10, SC_NS);
+      wait(50, SC_NS);
     }
 
     // cout << "source created for testbench" << endl;
@@ -141,50 +164,69 @@ SC_MODULE(Source) {
 
 SC_MODULE(testbench) {
   SC_HAS_PROCESS(testbench);
-  flex_sim flex;
+  relay_sim relay;
   Source src;
 
   sc_clock clk;
-  sc_signal< sc_biguint<1> > flex_wr_signal;
-  sc_signal< sc_biguint<1> > flex_rd_signal;
-  sc_signal< sc_biguint<32> > flex_addr_signal;
-  sc_signal< sc_biguint<8> > flex_data_signal[16];
+
+  sc_signal< sc_biguint<1> > relay_sim_relay_func_run_in_signal;
+  sc_signal< sc_biguint<8> > relay_sim_relay_func_id_signal;
+  sc_signal< sc_biguint<8> > relay_sim_relay_data_in_signal;
+  sc_signal< sc_biguint<8> > relay_sim_data_in_batch_signal;
+  sc_signal< sc_biguint<8> > relay_sim_data_in_channel_signal;
+  sc_signal< sc_biguint<32> > relay_sim_data_in_y_signal;
+  sc_signal< sc_biguint<32> > relay_sim_data_in_x_signal;
+  sc_signal< sc_biguint<8> > relay_sim_pool_size_y_signal;
+  sc_signal< sc_biguint<8> > relay_sim_pool_size_x_signal;
+  sc_signal< sc_biguint<8> > relay_sim_strides_y_in_signal;
+  sc_signal< sc_biguint<8> > relay_sim_strides_x_in_signal;
+  sc_signal< sc_biguint<8> > relay_sim_padding_in_y_signal;
+  sc_signal< sc_biguint<8> > relay_sim_padding_in_x_signal;
+  sc_signal< sc_biguint<8> > relay_sim_layout_in_signal;
+  sc_signal< sc_biguint<8> > relay_sim_ceil_mode_in_signal;
 
   testbench(sc_module_name name)
   : sc_module(name),
     clk("clk", 1, SC_NS),
     src("source"),
-    flex("flexnlp")
-  {
+    relay("flexnlp")
+    {
     // binding the signals from the source
     src.clk(clk);
-    src.flex_wr_in(flex_wr_signal);
-    src.flex_rd_in(flex_rd_signal);
-    src.flex_addr_in(flex_addr_signal);
-    for (int i = 0; i < 16; i++) {
-      src.flex_data_in[i](flex_data_signal[i]);
-    }
+
+    src.relay_sim_relay_func_run_in_in(relay_sim_relay_func_run_in_signal);
+    src.relay_sim_relay_func_id_in(relay_sim_relay_func_id_signal);
+    src.relay_sim_relay_data_in_in(relay_sim_relay_data_in_signal);
+    src.relay_sim_data_in_batch_in(relay_sim_data_in_batch_signal);
+    src.relay_sim_data_in_channel_in(relay_sim_data_in_channel_signal);
+    src.relay_sim_data_in_y_in(relay_sim_data_in_y_signal);
+    src.relay_sim_data_in_x_in(relay_sim_data_in_x_signal);
+    src.relay_sim_pool_size_y_in(relay_sim_pool_size_y_signal);
+    src.relay_sim_pool_size_x_in(relay_sim_pool_size_x_signal);
+    src.relay_sim_strides_y_in_in(relay_sim_strides_y_in_signal);
+    src.relay_sim_strides_x_in_in(relay_sim_strides_x_in_signal);
+    src.relay_sim_padding_in_y_in(relay_sim_padding_in_y_signal);
+    src.relay_sim_padding_in_x_in(relay_sim_padding_in_x_signal);
+    src.relay_sim_layout_in_in(relay_sim_layout_in_signal);
+    src.relay_sim_ceil_mode_in_in(relay_sim_ceil_mode_in_signal);
+
 
     // binding the signals for the model
-    flex.flex_sim_if_axi_rd_in(flex_rd_signal);
-    flex.flex_sim_if_axi_wr_in(flex_wr_signal);
-    flex.flex_sim_addr_in_in(flex_addr_signal);
-    flex.flex_sim_data_in_0_in(flex_data_signal[0]);
-    flex.flex_sim_data_in_1_in(flex_data_signal[1]);
-    flex.flex_sim_data_in_2_in(flex_data_signal[2]);
-    flex.flex_sim_data_in_3_in(flex_data_signal[3]);
-    flex.flex_sim_data_in_4_in(flex_data_signal[4]);
-    flex.flex_sim_data_in_5_in(flex_data_signal[5]);
-    flex.flex_sim_data_in_6_in(flex_data_signal[6]);
-    flex.flex_sim_data_in_7_in(flex_data_signal[7]);
-    flex.flex_sim_data_in_8_in(flex_data_signal[8]);
-    flex.flex_sim_data_in_9_in(flex_data_signal[9]);
-    flex.flex_sim_data_in_10_in(flex_data_signal[10]);
-    flex.flex_sim_data_in_11_in(flex_data_signal[11]);
-    flex.flex_sim_data_in_12_in(flex_data_signal[12]);
-    flex.flex_sim_data_in_13_in(flex_data_signal[13]);
-    flex.flex_sim_data_in_14_in(flex_data_signal[14]);
-    flex.flex_sim_data_in_15_in(flex_data_signal[15]);
+    relay.relay_sim_relay_func_run_in_in(relay_sim_relay_func_run_in_signal);
+    relay.relay_sim_relay_func_id_in(relay_sim_relay_func_id_signal);
+    relay.relay_sim_relay_data_in_in(relay_sim_relay_data_in_signal);
+    relay.relay_sim_data_in_batch_in(relay_sim_data_in_batch_signal);
+    relay.relay_sim_data_in_channel_in(relay_sim_data_in_channel_signal);
+    relay.relay_sim_data_in_y_in(relay_sim_data_in_y_signal);
+    relay.relay_sim_data_in_x_in(relay_sim_data_in_x_signal);
+    relay.relay_sim_pool_size_y_in(relay_sim_pool_size_y_signal);
+    relay.relay_sim_pool_size_x_in(relay_sim_pool_size_x_signal);
+    relay.relay_sim_strides_y_in_in(relay_sim_strides_y_in_signal);
+    relay.relay_sim_strides_x_in_in(relay_sim_strides_x_in_signal);
+    relay.relay_sim_padding_in_y_in(relay_sim_padding_in_y_signal);
+    relay.relay_sim_padding_in_x_in(relay_sim_padding_in_x_signal);
+    relay.relay_sim_layout_in_in(relay_sim_layout_in_signal);
+    relay.relay_sim_ceil_mode_in_in(relay_sim_ceil_mode_in_signal);
 
     SC_THREAD(run);
   }
@@ -200,44 +242,8 @@ SC_MODULE(testbench) {
     std::cout << "@" << sc_time_stamp() << " ********* simulation start *********" << std::endl;
     wait(10, SC_NS);
 
-    while (undone) {
-      if (flex.flex_sim_addr_in.to_int() == stop_addr) {
-          undone = false;
-      }
-
-      fout << "@ " << sc_time_stamp() << '\t';
-      fout << "is write? :" << '\t' << flex.flex_sim_if_axi_wr_in << '\t';
-      fout << "addr in:" << '\t' << hex << flex.flex_sim_addr_in << '\t';
-      fout << "data in:" << '\t';
-      for (int k=0; k < 16; k++) {
-        fout << hex << flex_data_signal[15-k] << ' ';
-      }
-      fout << endl;
-      fout << "flex status:" << '\t';
-      fout << "reduce valid: " << '\t' << flex.flex_sim_gb_layer_reduce_is_valid << '\t';
-      fout << "grouping num: " << '\t' << flex.flex_sim_gb_layer_reduce_grouping_num << '\n' << endl;
-      wait(10, SC_NS);
-    }
-
-    wait(10000, SC_NS);
-    fout << "********* output for large buffer ***********" << endl;
-    
-    int entry_addr;
-    int index;
-    for (int j = 0; j < 0xB00; j++) {
-        entry_addr = 16*j;
-        fout << "large buffer @ addr:" << '\t';
-        fout << hex << entry_addr << '\t';
-        fout << "data:" << '\t';
-        for (int k = 0; k < 16; k++) {
-            index = 16*j + 15 - k;
-            fout << hex << flex.flex_sim_gb_core_large_buffer[index] << ' ';
-        }
-        fout << endl;
-    }
 
     wait(1000, SC_NS);
-    cout << "test for accessing flex:  " << hex << flex.flex_sim_gb_core_large_buffer[12] << endl;
     std::cout << "@" << sc_time_stamp() << " *********     sc_stop      *********" << std::endl;
     sc_stop();
   }
